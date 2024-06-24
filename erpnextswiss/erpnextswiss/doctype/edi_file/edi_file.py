1c1c21fa8888d387a4230947617adc88a997237b
Simon Krieger <150357267+krieger00@users.noreply.github.com>
2024-06-24 06:33:47 +0200

    Update edi_file.py

--- erpnextswiss/erpnextswiss/doctype/edi_file/edi_file.py
+++ erpnextswiss/erpnextswiss/doctype/edi_file/edi_file.py
@@ -9,7 +9,7 @@ from erpnextswiss.erpnextswiss.edi import download_pricat, download_desadv, get_
 from erpnextswiss.erpnextswiss.attach_pdf import create_folder
 from frappe.utils import cint
 from frappe.utils.file_manager import save_file
-from frappe.email.queue import bulk
+from frappe.email.bulk import send
 
 class EDIFile(Document):
     def on_submit(self):
